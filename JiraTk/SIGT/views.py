ac7ad05d2af52056979b6d6fa83e7ae219a0d997
IvanTomasevich <tomasevic.ind@gmail.com>
2023-06-14 11:47:36 -0300

    Login Logout avatar

--- JiraTk/SIGT/views.py
+++ JiraTk/SIGT/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.
 
 
 def sigt_view(request):
     return render(request, "sigt.html", {})
+
+
