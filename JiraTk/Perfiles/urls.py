376aec98a327e997b74360bb6978d0a591b4ba8d
IvanTomasevich <tomasevic.ind@gmail.com>
2023-06-21 22:48:33 -0300

    minor update profile

--- JiraTk/Perfiles/urls.py
+++ JiraTk/Perfiles/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('login/', login_view, name="login"),
     path('logout/', CustomLogoutView.as_view(), name="logout"),
     # URLS de Perfil
-    path('editar-mi-perfil/', MiPerfilUpdateView.as_view(), name="editar_perfil"),
+    path('profile/', MiPerfilUpdateView.as_view(), name="editar_perfil"),
     path('agregar-avatar/', agregar_avatar, name="agregar_avatar"),
 ]
